02e44777de737e45b86090586dbea2c8033190d3
Aleksandr-Abramov <ale19850505@gmail.com>
2023-02-14 20:29:06 +0300

    feature: сверстал BurgerIngridients

--- src/components/app-header/AppHeader.jsx
+++ src/components/app-header/AppHeader.jsx
@@ -12,10 +12,10 @@ const AppHeader = () => {
     <div className="wrapper">
       <nav className="nav">
         <ul className={ styles.menu }>
-          <li className={`${styles.menuItem}`}><a href="/" className={ styles.menuLink }><BurgerIcon/><span className={`text text_type_main-default text_color_inactive pl-2`}>Конструктор</span></a></li>
-          <li className={`${styles.menuItem}`}><a href="/" className={ styles.menuLink }><ListIcon/><span className={`text text_type_main-default text_color_inactive pl-2`}>Лента заказов</span></a></li>
+          <li className={`${styles.menuItem}`}><a href="/" className={ styles.menuLink }><BurgerIcon type="secondary"/><span className={`text text_type_main-default text_color_inactive pl-2`}>Конструктор</span></a></li>
+          <li className={`${styles.menuItem}`}><a href="/" className={ styles.menuLink }><ListIcon type="secondary"/><span className={`text text_type_main-default text_color_inactive pl-2`}>Лента заказов</span></a></li>
           <li className={`${styles.menuItem}`}><a href="/" className={ styles.menuLink }><Logo/></a></li>
-          <li className={`${styles.menuItem}`}><a href="/" className={ styles.menuLink }><ProfileIcon/><span className={`text text_type_main-default text_color_inactive pl-2`}>Личный кабинет</span></a></li>
+          <li className={`${styles.menuItem}`}><a href="/" className={ styles.menuLink }><ProfileIcon type="secondary"/><span className={`text text_type_main-default text_color_inactive pl-2`}>Личный кабинет</span></a></li>
         </ul>
       </nav>
     </div>
--- src/components/burger-ingredients/BurgerIngredients.jsx
+++ src/components/burger-ingredients/BurgerIngredients.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styles from "./burger-ingredients.module.css";
+import TabContainer from "../tab-container/TabContainer";
+import { data } from '../../utils/data';
+
 import {
     Counter,
     CurrencyIcon,
@@ -8,12 +11,50 @@ import {
   } from "@ya.praktikum/react-developer-burger-ui-components";
 
 const BurgerIngredients = () => {
+
     return (
-        <section className="burger-ingridients ">
+        <section className={styles.section}>
+            <h1 className={`mt-10 mb-5 text text_type_main-large`}>Соберите бургер</h1>
+            <TabContainer/>
+            <div className={styles.scroll}>
+            <div className={styles.container}>
+            <h2 className={`mb-6 text text_type_main-medium ${styles.title2}`}>Булки</h2>
+                {data.filter((item) => item.type === "bun").map((item) => (
+                    <div className={styles.containerIngridients} key={item._id}>
+                        <img src={item.image} alt={item.name} />
+                        <div className={styles.price}><span className={`text text_type_digits-default ${styles.priceSpan}`}>{item.price}</span><CurrencyIcon/></div>
+                        <h3 className="text text_type_main-default">{item.name}</h3>
+                        <Counter count={1} size="small"/>
+                </div>
+                ))}
+            </div> 
+
+            <div className={styles.container}>
+            <h2 className={`mb-6 text text_type_main-medium ${styles.title2}`}>Соусы</h2>
+                {data.filter((item) => item.type === "sauce").map((item) => (
+                    <div className={styles.containerIngridients} key={item._id}>
+                        <img src={item.image} alt={item.name} />
+                        <div className={styles.price}><span className={`text text_type_digits-default ${styles.priceSpan}`}>{item.price}</span><CurrencyIcon/></div>
+                        <h3 className={`text text_type_main-default`}>{item.name}</h3>
+                        <Counter count={1} size="small"/>
+                </div>
+                ))}
+            </div> 
+
+
+            <div className={styles.container}>
+            <h2 className={`mb-6 text text_type_main-medium ${styles.title2}`}>Соусы</h2>
+                {data.filter((item) => item.type === "main").map((item) => (
+                    <div className={styles.containerIngridients} key={item._id}>
+                        <img src={item.image} alt={item.name} />
+                        <div className={styles.price}><span className={`text text_type_digits-default ${styles.priceSpan}`}>{item.price}</span><CurrencyIcon/></div>
+                        <h3 className="text text_type_main-default">{item.name}</h3>
+                        <Counter count={1} size="small"/>
+                </div>
+                ))}
+            </div> 
+            </div>
             
-            <Counter count={1} size="default" extraClass="m-1" />
-            <Counter count={233} size="small" />
-            <CurrencyIcon type="primary" />
         </section>
     );
 }
