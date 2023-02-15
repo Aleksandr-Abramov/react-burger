2b95eb2d0cd01bb7e56ca9eb40fc9cff3a99e457
Aleksandr-Abramov <ale19850505@gmail.com>
2023-02-15 15:17:31 +0300

    feature: изменил BurgerIngridients, сверстал BurgerIngridient

---
+++ src/components/burger-ingredient/BurgerIngredient.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./burger-ingredients.module.css";
+
+import {
+  Counter,
+  CurrencyIcon,
+  Tab,
+  ListIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+
+const BurgerIngredient = ({ ingredients, title = `Булки` }) => {
+  console.log(ingredients);
+  return (
+    <>
+      <h2 className={`${styles.title} text text_type_main-medium`}>{title}</h2>
+      {ingredients.map((item) => {
+        return (
+          <div className={styles.container} key={item._id}>
+            <img src={item.image} alt={item.name} className={styles.img} />
+            <div className={styles.priceContainer}>
+              <span className={`${styles.span} text text_type_digits-default`}>
+                {item.price}
+              </span>
+              <CurrencyIcon type="primary" />
+            </div>
+            <p className={`${styles.text} text text_type_main-default`}>
+              {item.name}
+            </p>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default BurgerIngredient;
--- src/components/burger-ingredients/BurgerIngredients.jsx
+++ src/components/burger-ingredients/BurgerIngredients.jsx
@@ -1,62 +1,36 @@
 import React from "react";
 import styles from "./burger-ingredients.module.css";
 import TabContainer from "../tab-container/TabContainer";
-import { data } from '../../utils/data';
+import BurgerIngredient from "../burger-ingredient/BurgerIngredient";
+import { data } from "../../utils/data";
 
 import {
-    Counter,
-    CurrencyIcon,
-    Tab,
-    ListIcon,
-  } from "@ya.praktikum/react-developer-burger-ui-components";
+  Counter,
+  CurrencyIcon,
+  Tab,
+  ListIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 
 const BurgerIngredients = () => {
+  const bun = data.filter((item) => item.type === "bun").map((item) => item);
+  const main = data.filter((item) => item.type === "main").map((item) => item);
+  const sauce = data
+    .filter((item) => item.type === "sauce")
+    .map((item) => item);
 
-    return (
-        <section className={styles.section}>
-            <h1 className={`mt-10 mb-5 text text_type_main-large`}>Соберите бургер</h1>
-            <TabContainer/>
-            <div className={styles.scroll}>
-            <div className={styles.container}>
-            <h2 className={`mb-6 text text_type_main-medium ${styles.title2}`}>Булки</h2>
-                {data.filter((item) => item.type === "bun").map((item) => (
-                    <div className={styles.containerIngridients} key={item._id}>
-                        <img src={item.image} alt={item.name} />
-                        <div className={styles.price}><span className={`text text_type_digits-default ${styles.priceSpan}`}>{item.price}</span><CurrencyIcon/></div>
-                        <h3 className="text text_type_main-default">{item.name}</h3>
-                        <Counter count={1} size="small"/>
-                </div>
-                ))}
-            </div> 
-
-            <div className={styles.container}>
-            <h2 className={`mb-6 text text_type_main-medium ${styles.title2}`}>Соусы</h2>
-                {data.filter((item) => item.type === "sauce").map((item) => (
-                    <div className={styles.containerIngridients} key={item._id}>
-                        <img src={item.image} alt={item.name} />
-                        <div className={styles.price}><span className={`text text_type_digits-default ${styles.priceSpan}`}>{item.price}</span><CurrencyIcon/></div>
-                        <h3 className={`text text_type_main-default`}>{item.name}</h3>
-                        <Counter count={1} size="small"/>
-                </div>
-                ))}
-            </div> 
-
-
-            <div className={styles.container}>
-            <h2 className={`mb-6 text text_type_main-medium ${styles.title2}`}>Соусы</h2>
-                {data.filter((item) => item.type === "main").map((item) => (
-                    <div className={styles.containerIngridients} key={item._id}>
-                        <img src={item.image} alt={item.name} />
-                        <div className={styles.price}><span className={`text text_type_digits-default ${styles.priceSpan}`}>{item.price}</span><CurrencyIcon/></div>
-                        <h3 className="text text_type_main-default">{item.name}</h3>
-                        <Counter count={1} size="small"/>
-                </div>
-                ))}
-            </div> 
-            </div>
-            
-        </section>
-    );
-}
+  return (
+    <section className={styles.section}>
+      <h1 className={`mt-10 mb-10 text text_type_main-large`}>
+        Соберите бургер
+      </h1>
+      <TabContainer />
+      <div className={`${styles.container}`}>
+        <BurgerIngredient ingredients={bun} title="Булки" />
+        <BurgerIngredient ingredients={sauce} title="Соусы" />
+        <BurgerIngredient ingredients={main} title="Ингридиенты" />
+      </div>
+    </section>
+  );
+};
 
 export default BurgerIngredients;
